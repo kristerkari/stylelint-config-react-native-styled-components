0dc1f7a88a0051158601a2a999750643f213cf4f
Krister Kari <krister@sent.at>
2023-02-13 18:24:17 -0300

    Remove disabling of no-missing-end-of-source-newline

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const rules = {
   "value-no-vendor-prefix": true,
   "property-no-vendor-prefix": true,
   "no-empty-source": null,
-  "no-missing-end-of-source-newline": null,
   "at-rule-whitelist": [
     [],
     {
